430a196fa23c3ad444e69f6f37cb14f9cca60a13
Evgeniy9000 <disabled16@mai.ru>
2021-11-19 10:15:50 +0300

    js

---
+++ js/search.js
@@ -0,0 +1,13 @@
+const search = function () {
+    const input = document.querySelector('.search-block > input')
+    const searchBtn = document.querySelector('.search-block > input')
+
+
+    input.addEventListener('input', (event) => {
+        console.log(event.target.value);
+
+
+    })
+}
+
+search()
